d62d0d27141ec75b62cd0b2693362c826e3180b4
MaxQiu <yueyiqiu178@gmail.com>
2023-12-11 17:22:26 +0800

    加入Run2_1.java

---
+++ src/main/java/com/multi/thread/chapter9/section2/example2/Run2_1.java
@@ -0,0 +1,47 @@
+package com.multi.thread.chapter9.section2.example2;
+
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class Run2_1 {
+	// 线程数量小于等于corePoolSize
+	// keepAliveTime大于5时也不清除空闲线程
+	// 因为空闲线程在corePool中
+	// corePool中的线程是不能删除的
+	// 所以keepAliveTime参数无效
+	public static void main(String[] args) throws InterruptedException {
+		Runnable runnable = new Runnable() {
+			@Override
+			public void run() {
+				try {
+					System.out.println(Thread.currentThread().getName() + " run!" + System.currentTimeMillis());
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		};
+		ThreadPoolExecutor executor = new ThreadPoolExecutor(7, 8, 5, TimeUnit.SECONDS, new LinkedBlockingQueue());
+		executor.execute(runnable); // 1
+		executor.execute(runnable); // 2
+		executor.execute(runnable); // 3
+		executor.execute(runnable); // 4
+
+		executor.execute(runnable); // 5
+		executor.execute(runnable); // 6
+		executor.execute(runnable); // 7
+		Thread.sleep(300);
+		System.out.println("A executor.getCorePoolSize()=" + executor.getCorePoolSize());
+		System.out.println("A executor.getMaximumPoolSize()=" + executor.getMaximumPoolSize());
+		System.out.println("A executor.getPoolSize()=" + executor.getPoolSize());
+		System.out.println("A executor.getQueue().size()=" + executor.getQueue().size());
+		Thread.sleep(10000);
+		System.out.println("10秒后打印结果");
+		System.out.println("B executor.getCorePoolSize()=" + executor.getCorePoolSize());
+		System.out.println("B executor.getMaximumPoolSize()=" + executor.getMaximumPoolSize());
+		System.out.println("B executor.getPoolSize()=" + executor.getPoolSize());
+		System.out.println("B executor.getQueue().size()=" + executor.getQueue().size());
+	}
+	// 按钮呈红色，因为池中还有线程在等待任务
+}
